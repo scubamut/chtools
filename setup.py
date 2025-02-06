bae9fd0362eae6379aecb5623e2f451796dba537
scubamut <scubamut@gmail.com>
2025-02-06 08:49:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,17 +21,3 @@ setup(
     python_requires='>=3.10'
 )
 
-from distutils.core import setup
-
-# setup(
-#     name='fintools',
-#     version='1.0.3',
-#     author='Dave Gilbert',
-#     author_email='scubamut @gmail.com',
-#     packages=['fintools'],
-#     scripts=[],
-#     url='http://pypi.python.org/pypi/fintools/',
-#     license='LICENSE.txt',
-#     description='Toolset for developing and backtesting asset allocation strategies',
-#     long_description=open('README.md').read(),
-# )
