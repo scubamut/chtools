1f1eca0235254e2735f5fbeba030c0793a858197
scubamut <scubamut@gmail.com>
2025-02-06 08:48:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='A package to interact with the UK Companies House API',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/scubamut/companies-house-project',
+    url='http://pypi.python.org/pypi/chtools/',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
@@ -20,3 +20,18 @@ setup(
     ],
     python_requires='>=3.10'
 )
+
+from distutils.core import setup
+
+# setup(
+#     name='fintools',
+#     version='1.0.3',
+#     author='Dave Gilbert',
+#     author_email='scubamut @gmail.com',
+#     packages=['fintools'],
+#     scripts=[],
+#     url='http://pypi.python.org/pypi/fintools/',
+#     license='LICENSE.txt',
+#     description='Toolset for developing and backtesting asset allocation strategies',
+#     long_description=open('README.md').read(),
+# )
